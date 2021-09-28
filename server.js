5ceef57a8c1b8d3b973f8a66a89e27f2d98b5cd7
Irina Boiko <irinaboiko95@gmail.com>
2021-09-28 12:37:04 +0300

    add ejs and morgan

--- server.js
+++ server.js
@@ -1,30 +1,62 @@
 const express = require("express");
 const path = require("path");
+const morgan = require("morgan");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 const PORT = 3000;
 
-const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 
 app.listen(PORT, (error) => {
   error ? console.log(error) : console.log(`Listening on port ${PORT}`);
 });
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+app.use(express.static('styles'));
+
 app.get('/', (request, response) => {
-  response.sendFile(createPath("index"));
+  const title = "Home";
+
+  response.render(createPath("index"), {title});
 });
 
 app.get('/contacts', (request, response) => {
-  response.sendFile(createPath("contacts"));
+  const title = "Contacts";
+  const contacts = [
+    {name: 'YouTube', link: "http://youtube.com/YauhenKavalchuk"},
+    {name: 'GitHub', link: "http://github.com/YauhenKavalchuk"},
+    {name: 'Twitter', link: "http://twitter.com/YauhenKavalchuk"},
+  ];
+
+  response.render(createPath("contacts"), { contacts, title });
 });
 
-app.get('/about-us', (request, response) => {
-  response.redirect("contacts");
+app.get('/posts/:id', (request, response) => {
+  const title = "Post";
+
+  response.render(createPath("post"), {title});
+});
+
+app.get('/posts', (request, response) => {
+  const title = "Posts";
+
+  response.render(createPath("posts"), {title});
+});
+
+app.get('/add-post', (request, response) => {
+  const title = "Add post";
+
+  response.render(createPath("add-post"), {title});
 });
 
 app.use((request, response) => {
+  const title = "Error";
+
   response
     .status(404)
-    .sendFile(createPath('error'));
+    .render(createPath('error'), {title});
 });
